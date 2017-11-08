85950f38d54119769c9720c24b308fe3f426bc56
david <c14464428@mydit.ie>
2017-11-08 09:44:05 +0000

    Implemented Firebase User login via Facebook

--- app/src/main/java/com/thedavehunt/eko/MainActivity.java
+++ app/src/main/java/com/thedavehunt/eko/MainActivity.java
@@ -6,29 +6,47 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends Activity {
 
     //used to register/ssignin users
     private FirebaseAuth auth;
+    private  CallbackManager mCallbackManager;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
+    /*
     private EditText emailEdt;
     private EditText passwordEdt;
     private Button registerBtn;
     private Button signBtn;
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +56,52 @@ public class MainActivity extends Activity {
         //get Firebase auth instance
         auth=FirebaseAuth.getInstance();
 
+        /*
         emailEdt = (EditText)findViewById(R.id.emailEdit);
         passwordEdt = (EditText)findViewById(R.id.passwordEdit);
         registerBtn = (Button)findViewById(R.id.registerBtn);
         signBtn = (Button)findViewById(R.id.signinBtn);
+        */
 
 
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.loginButtonFacebook);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            //signed into facebook account
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent landingpage = new Intent(MainActivity.this,landingPage.class);
+                    startActivity(landingpage);
+
+                } else {
+                    Log.d("TG", "SIGNED OUT");
+                }
+            }
+        };
+
+
+        /*
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,7 +118,59 @@ public class MainActivity extends Activity {
                 signinUser();
             }
         });
+        */
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser user = auth.getCurrentUser();
+        Toast.makeText(MainActivity.this,"Welcome " + user.getDisplayName(),Toast.LENGTH_SHORT).show();
+        if(user!=null) {
+            Intent landingpage = new Intent(MainActivity.this, landingPage.class);
+            startActivity(landingpage);
+        }
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+
+                            Toast.makeText(MainActivity.this,"Welcome " + user.getDisplayName(),Toast.LENGTH_SHORT).show();
+                            Intent landingpage = new Intent(MainActivity.this,landingPage.class);
+                            startActivity(landingpage);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Unable to authenticate user",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+
+    //code for signing in via an email and password
+    /*
 
     //method for registering users
     private void registerUser() {
@@ -112,6 +222,6 @@ public class MainActivity extends Activity {
             }
         });
     }
-
+*/
 
 }
